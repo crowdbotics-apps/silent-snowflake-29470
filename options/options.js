1b07ec303e0f87ab6a4ff05a98f1cf3258b2f10f
Crowdbotics <team@crowdbotics.com>
2021-08-04 19:16:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "silent_snowflake_29470Identifier",
+  url: "https://silent_snowflake_29470Identifier.botics.co",
+  api: "https://silent_snowflake_29470Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
